eb0c9a37e06231eca116554bcab1514431943ad8
Nguyễn Tấn Dũng <118035439+tandung170302@users.noreply.github.com>
2022-11-15 10:13:09 +0700

    Add files via upload

---
+++ category.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+<table > 
+	    <tr > 
+	        <th>Name</th> 
+	        <th>Action</th> 
+	    </tr> 
+	    <tr> 
+	        <td>CCTV</td> 
+	        <td>Edit</td> 
+	        <td><a href="#">Remove</a></td> 
+	    </tr> 
+        <tr> 
+	        <td>Computer Set</td> 
+	        <td>Edit</td> 
+	        <td><a href="#">Remove</a></td> 
+	    </tr> 
+        <tr> 
+	        <td>Hard Disk</td> 
+	        <td>Edit</td> 
+	        <td><a href="#">Remove</a></td> 
+	    </tr> 
+        <tr> 
+	        <td>Keyboard</td> 
+	        <td>Edit</td> 
+	        <td><a href="#">Remove</a></td> 
+	    </tr> 
+        <tr> 
+	        <td>Laptops</td> 
+	        <td>Edit</td> 
+	        <td><a href="#">Remove</a></td> 
+	    </tr> 
+        <tr> 
+	        <td>Memory</td> 
+	        <td>Edit</td> 
+	        <td><a href="#">Remove</a></td> 
+	    </tr> 
+        <tr> 
+	        <td>Mouse</td> 
+	        <td>Edit</td> 
+	        <td><a href="#">Remove</a></td> 
+	    </tr> 
+	</table>
+</body>
+</html>
---
+++ product.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>shopping cart</title>
+</head>
+
+<style>
+    a {color: #48577D; text-decoration: none;} 
+  
+  a:hover {text-decoration: underline;} 
+    
+  h1, h2 {margin-bottom: 15px} 
+    
+  h1 {font-size: 18px;} 
+  h2 {font-size: 16px} 
+      #main table { 
+              width: 480px; 
+          } 
+            
+              #main table th { 
+                  padding: 10px; 
+                  background-color: #48577D; 
+                  color: #fff; 
+                  text-align: left; 
+              } 
+                
+              #main table td { 
+                  padding: 5px; 
+              } 
+              #main table tr { 
+                  background-color: #d3dcf2; 
+              }
+              body { 
+    font-family: Verdana; 
+    font-size: 12px; 
+    color: #444; 
+} 
+  
+a {color: #48577D; text-decoration: none;} 
+  
+a:hover {text-decoration: underline;} 
+  
+h1, h2 {margin-bottom: 15px} 
+  
+h1 {font-size: 18px;} 
+h2 {font-size: 16px} 
+  
+#container { 
+    width: 700px; 
+    margin: 150px auto; 
+    background-color: #eee; 
+    padding:15px; 
+    overflow: hidden; 
+} 
+  
+    #main { 
+        width: 490px; 
+        float: left; 
+    } 
+      
+        #main table { 
+            width: 480px; 
+        } 
+          
+            #main table th { 
+                padding: 10px; 
+                background-color: #48577D; 
+                color: #fff; 
+                text-align: left; 
+            } 
+              
+            #main table td { 
+                padding: 5px; 
+            } 
+              
+            #main table tr { 
+                background-color: #d3dcf2; 
+            } 
+      
+    #sidebar { 
+        width: 200px; 
+        float: left; 
+    }
+</style>
+<body>
+<h1>Product List</h1> 
+	<table> 
+	    <tr> 
+	        <th>Name</th> 
+	        <th>Description</th> 
+	        <th>Price</th> 
+	        <th>Action</th> 
+	    </tr> 
+	    <tr> 
+	        <td>Product 1</td> 
+	        <td>Some random description</td> 
+	        <td>15 $</th> 
+	        <td><a href="#">Add to cart</a></td> 
+	    </tr> 
+	     <tr> 
+	        <td>Product 2</td> 
+	        <td>Some random description</td> 
+	        <td>25 $</th> 
+	        <td><a href="#">Add to cart</a></td> 
+	    </tr> 
+	</table>
+</body>
+</html>
